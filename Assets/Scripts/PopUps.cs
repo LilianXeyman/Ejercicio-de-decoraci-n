8bb3ed2bca486be516b794649131f06744bab4df
Elisa Herrera <elisa0101232478@gmail.com>
2024-11-07 12:16:05 +0000

    Interfaz
    Creación de la interfaz + escenario
    Algunas animaciones

---
+++ Assets/Scripts/PopUps.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUps : MonoBehaviour
+{
+    [SerializeField]
+    float posicionFinal;
+    [SerializeField]
+    LeanTweenType animeCurv;
+    [SerializeField]
+    GameObject popUpCreacionObjetos;
+    [SerializeField]
+    float posicionPopUpCreaObj;
+    [SerializeField]
+    float movimientoInicialObjetos;
+    [SerializeField]
+    GameObject objetos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PopUpMenu()
+    {
+        LeanTween.moveLocalY(gameObject, posicionFinal, 1f).setEase(animeCurv);
+        LeanTween.moveLocalX(popUpCreacionObjetos, 1185, 1f).setEase(animeCurv).setOnComplete(() => {
+            popUpCreacionObjetos.SetActive(false);
+        });
+        
+    }
+    public void Crear()
+    {
+        popUpCreacionObjetos.SetActive(true);
+        LeanTween.moveLocalY(gameObject, 0, 1f).setEase(animeCurv).setOnComplete(()=> { 
+        LeanTween.moveLocalX(popUpCreacionObjetos, posicionPopUpCreaObj, 1f).setEase(animeCurv);
+        });
+        
+        
+    }
+    public void Subir()
+    {
+        LeanTween.moveLocalY(objetos, movimientoInicialObjetos, 1f).setEase(animeCurv);//Me quede por aquí. El objetivo es que suban los objetos y se coloquen en el menú de cración de objetos
+    }
+    public void Bajar()
+    { 
+    
+    }
+    public void Rotar()
+    {
+        LeanTween.moveLocalY(gameObject, 0, 1f).setEase(animeCurv);
+    }
+    public void Mover()
+    {
+        LeanTween.moveLocalY(gameObject, 0, 1f).setEase(animeCurv);
+    }
+    public void Eliminar()
+    {
+        LeanTween.moveLocalY(gameObject, 0, 1f).setEase(animeCurv);
+    }
+
+}
